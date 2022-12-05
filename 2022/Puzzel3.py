deb4c8ac4afe6e6ef8a1fc397fa9fa7027db6dd3
tgarbers <t.garbers@kh-itzehoe.de>
2022-12-05 11:28:46 +0100

    added solution for day 3

---
+++ 2022/Puzzel3.py
@@ -0,0 +1,32 @@
+# TODO use import for puzzel input
+
+f = open("puzzelinput3.txt", "r")
+
+lines = f.read()
+inputData = lines.split('\n')
+
+
+#Puzzel Solution 1
+
+items = []
+
+for l in inputData:
+	middle = int(len(l) / 2)
+	for f in l[:middle]:
+		if l[middle:].find(f) >= 0:
+			items.append(f)
+			break
+
+print(sum(list(map('@abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.find, items)))) # @ in front to increase index by one
+
+#Puzzel Solution 2
+
+badges = []
+for g in range(0, len(inputData), 3):
+	for b in inputData[g]:
+		if inputData[g+1].find(b) >= 0:
+			if inputData[g+2].find(b) >= 0:
+				badges.append(b)
+				break
+
+print(sum(list(map('@abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.find, badges))))
