12091136779a77aff4afce40988175380bb591ed
tgarbers <t.garbers@kh-itzehoe.de>
2022-12-13 14:57:04 +0100

    added proxy to importPuzzelData

--- 2022/ImportPuzzelData.py
+++ 2022/ImportPuzzelData.py
@@ -1,11 +1,22 @@
 import requests
+import json
 
-with open('./secret.txt') as f:
-  key = f.read()
+with open('./secret.json') as f:
+  j = json.load(f)
+print(j['session'])
   
-def get(year: int, day: int):
-  cookies = dict(session=key)
-  res = requests.get(f'https://adventofcode.com/{str(year)}/day/{str(day)}/input', cookies=cookies)
+proxies = {
+  'http':'http://172.21.255.254:8080',
+  'https':'http://172.21.255.254:8090'
+}
+
+def get(year: int, day: int, useProxy: bool):
+  cookies = dict(session=j['session'])
+  res = ''
+  if useProxy:
+    res = requests.get(f'https://adventofcode.com/{str(year)}/day/{str(day)}/input', cookies=cookies, auth=(j['proxyUser'], j['proxyPass']), proxies=proxies)
+  else:
+    res = requests.get(f'https://adventofcode.com/{str(year)}/day/{str(day)}/input', cookies=cookies)
   inputData = res.text
 
   return inputData
